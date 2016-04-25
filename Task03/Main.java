79f2cf6a65a6c7d4dc016bb627af38bb1b85e0bf
SirheiNichyparuk <Siarhei_Nichyparuk@epam.com>
2016-04-25 10:11:09 +0300

    Create Main.java

---
+++ Task03/Main.java
@@ -0,0 +1,17 @@
+package Task03;
+
+//3. Вычислить периметр и площадь прямоугольного треугольника по длинам а и b  двух катетов.
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Triangle triangle = new Triangle();
+
+		triangle.Calculate();
+
+		triangle.show();
+
+	}
+
+}
