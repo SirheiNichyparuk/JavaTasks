a57f83d814c84c5e7c996e1dc735ff5687b32111
SirheiNichyparuk <Siarhei_Nichyparuk@epam.com>
2016-04-25 10:08:33 +0300

    Added files via upload

---
+++ Task01/Main.java
@@ -0,0 +1,12 @@
+package Task01;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		calc.digitsSearching();
+		calc.Calculation();
+	}
+
+}
