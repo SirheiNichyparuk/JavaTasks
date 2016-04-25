556c6946638ff0644d819b5e0dd1db48569799cb
SirheiNichyparuk <Siarhei_Nichyparuk@epam.com>
2016-04-25 10:44:22 +0300

    Create Main.java

---
+++ Task10/Main.java
@@ -0,0 +1,13 @@
+package Task10;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		int n =8;
+		MatrixClass mc = new MatrixClass();
+		mc.show(mc.createArray(n));
+	}
+
+}
