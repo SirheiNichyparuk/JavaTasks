f268bc4dec2dfbc1ac74b7ee4ec0406b754ed830
SirheiNichyparuk <Siarhei_Nichyparuk@epam.com>
2016-05-02 11:54:04 +0300

    Create NoteBookProvider.java

---
+++ NoteBook/src/service/NoteBookProvider.java
@@ -0,0 +1,26 @@
+package service;
+
+import bean.NoteBook;
+
+public class NoteBookProvider {
+    private static NoteBook instance = null;
+
+    private NoteBookProvider() {
+    }
+
+    public static NoteBook getInstance() {
+        if (instance == null) {
+            instance = new NoteBook();
+        }
+        return instance;
+    }
+
+    public static NoteBook createNoteBook() {
+        instance = new NoteBook();
+        return instance;
+    }
+
+    public static void setNoteBook(NoteBook noteBook) {
+        instance = noteBook;
+    }
+}
