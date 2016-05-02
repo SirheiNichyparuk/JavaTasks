13a4553afd2e83866f928cea7ea9d0740ae72fc2
SirheiNichyparuk <Siarhei_Nichyparuk@epam.com>
2016-05-02 11:51:08 +0300

    Create CommandHelper.java

---
+++ NoteBook/src/controller/CommandHelper.java
@@ -0,0 +1,35 @@
+package controller;
+
+import command.Command;
+import command.impl.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandHelper {
+    private Map<CommandName, Command> commands = new HashMap<>();
+
+    public CommandHelper() {
+        commands.put(CommandName.CREATE_NOTEBOOK, new CreateNoteBookCommand());
+        commands.put(CommandName.ADD_NOTE, new AddNoteCommand());
+        commands.put(CommandName.SEARCH_BY_DATE, new SearchByDateCommand());
+        commands.put(CommandName.SEARCH_BY_TEXT, new SearchByTextCommand());
+        commands.put(CommandName.SHOW_NOTEBOOK, new GetNoteBookCommand());
+        commands.put(CommandName.READ_FILE, new ReadFileCommand());
+        commands.put(CommandName.WRITE_FILE, new WriteFileCommand());
+    }
+
+    public Command getCommand(String commandName) {
+        Command executeCommand = null;
+        try {
+            CommandName command = CommandName.valueOf(commandName);
+            executeCommand = commands.get(command);
+        } catch (IllegalArgumentException e) {
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+        return executeCommand;
+    }
+}
