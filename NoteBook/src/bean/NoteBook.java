c8f7fd8411333b04fceee374ba22e828010478d3
SirheiNichyparuk <Siarhei_Nichyparuk@epam.com>
2016-05-02 11:47:32 +0300

    Added files via upload

---
+++ NoteBook/src/bean/NoteBook.java
@@ -0,0 +1,57 @@
+package bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteBook implements Serializable {
+	private List<Note> noteList;
+
+	public NoteBook() {
+		this.noteList = new ArrayList<Note>();
+	}
+
+	public List<Note> getNoteList() {
+		return noteList;
+	}
+
+	public Note getNote(int i) {
+		return noteList.get(i);
+	}
+
+	public void setNoteList(ArrayList<Note> noteList) {
+		this.noteList = noteList;
+	}
+
+	public void addNote(Note note) {
+		this.noteList.add(note);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+
+		NoteBook noteBook = (NoteBook) obj;
+		if (noteList != null ? !noteList.equals(noteBook.noteList)
+				: noteBook.noteList != null) {
+			return false;
+		}
+		return true;
+
+	}
+
+	@Override
+	public int hashCode() {
+		return noteList != null ? noteList.hashCode() : 0;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getName() + "@" + "noteList: '" + noteList;
+	}
+}
---
+++ NoteBook/src/bean/Request.java
@@ -0,0 +1,41 @@
+package bean;
+
+public class Request {
+	private String command;
+	private String parameter1;
+	private String parameter2;
+
+	public Request() {
+	}
+
+	public Request(String command, String text, String date) {
+		this.command = command;
+		this.parameter1 = text;
+		this.parameter2 = date;
+	}
+
+	public String getCommandName() {
+		return command;
+	}
+
+	public void setCommandName(String command) {
+		this.command = command;
+	}
+
+	public String getParameter1() {
+		return parameter1;
+	}
+
+	public void setParameter1(String parameter1) {
+		this.parameter1 = parameter1;
+	}
+
+	public String getParameter2() {
+		return parameter2;
+	}
+
+	public void setParameter2(String parameter2) {
+		this.parameter2 = parameter2;
+	}
+
+}
